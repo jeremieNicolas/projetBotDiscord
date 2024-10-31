0eb07ea4588c13a11ad61dd3d7c83f5286e34c20
Jérémie Nicolas <94701868+jeremieNicolas@users.noreply.github.com>
2024-10-31 12:31:06 -0400

    Connexion du robot

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const [Client, GatewayIntentBits] = require('discord.js')
+
+const Client = new Client({
+    //Permissions du bot
+    Intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.messageContent
+    ],
+});
+
+Client.on("ready", () => {
+    console.log(`Bot logged in as ${client.user.username}!`);
+});
+
+client.login("")
